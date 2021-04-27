b0af6ef23a2c769ebddaa6f6c822a0b4bf80c87f
wxygcs <335487092@qq.com>
2021-04-27 21:16:15 +0800

    null

--- studentManger/src/test_01/StudentManager.java
+++ studentManger/src/test_01/StudentManager.java
@@ -35,10 +35,12 @@ public class StudentManager {
                         addStudent(arrayStudent);
                         break;
                     case 2:
-                        System.out.println("请输入要删除的学生学号：");
+                        //System.out.println("请输入要删除的学生学号：");
+                        delete(arrayStudent);
                         break;
                     case 3:
-                        System.out.println("请输入要修改的学生学号：");
+                        //System.out.println("请输入要修改的学生学号：");
+                        change(arrayStudent);
                         break;
                     case 4:
                         //System.out.println("查看所有学生");
@@ -53,7 +55,7 @@ public class StudentManager {
     }
 
     //添加学生
-    public static void addStudent(ArrayList<Student> array){
+    public static void addStudent(ArrayList<Student> array) {
         Scanner sc = new Scanner(System.in);
 
         //显示提示信息，提示要输入何种信息
@@ -83,16 +85,93 @@ public class StudentManager {
 
     }
 
-    //修改学生
+    //删除学生
+    public static void delete(ArrayList<Student> array) {
+        //显示提示信息
+        System.out.println("请输入要删除的学生学号：");
+
+        //键盘录入要删除的学生学号
+        Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+
+        //遍历集合，将对应学生对象从集合中删除
+        //删除前先判断输入的学号是否存在
+        int index = -1;     //用-1来初始化，在for循环里用index表示索引
+        for (int i = 0; i < array.size(); i++) {
+            Student s = array.get(i);
+            //字符串的内容是否相同用 .equals()
+            if (s.getNumber().equals(number)) {
+                //输入的学号和遍历到的其中一个的学号相同，就把索引值赋值给index
+                index = i;
+                break;
+            }
+        }
+        //判断index是否发生变化，因为索引不可能是负数，如果index还是-1，那就说明没有对应的学号存在
+        if (index == -1) {
+            System.out.println("输入的学号不存在！");
+        } else {
+            array.remove(index);
+            //给出删除成功提示
+            System.out.println("删除成功！");
+        }
+    }
 
     //查看所有学生信息
-    public static void show(ArrayList<Student> array){
-        //使用"\t"表示tab键的位置
-        System.out.println("学号\t\t姓名\t\t年龄\t\t居住的");
+    public static void show(ArrayList<Student> array) {
+        //判断集合中是否有数据
+        if (array.size() == 0) {
+            System.out.println("无信息，请先添加内容！");
+        } else {
+            //使用"\t"表示tab键的位置
+            System.out.println("学号\t\t姓名\t\t年龄\t\t居住的");
 
-        for(int i = 0 ; i < array.size() ; i++){
+            for (int i = 0; i < array.size(); i++) {
+                Student a = array.get(i);
+                System.out.println(a.getNumber() + "\t\t" + a.getName() + "\t" + a.getAge() + "岁\t" + a.getAddress());
+            }
+        }
+    }
+
+    //修改学生
+    public static void change(ArrayList<Student> array) {
+        //显示提示信息
+        System.out.println("请输入要修改的学生学号：");
+        //键盘录入要修改的学生学号
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        //遍历集合，修改对应的学生信息
+        //先判断输入的学号是否存在
+        int index = -1;
+        for (int i = 0; i < array.size(); i++) {
             Student a = array.get(i);
-            System.out.println(a.getNumber()+"\t\t"+a.getName()+"\t"+a.getAge()+"岁\t"+a.getAddress());
+            if (a.getNumber().equals(s)) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            System.out.println("输入的学号不存在！");
+        } else {
+            //输入修改后的信息
+            System.out.println("请输入修改后的学号：");
+            String newNumber = sc.nextLine();
+            System.out.println("请输入修改后的姓名：");
+            String newName = sc.nextLine();
+            System.out.println("请输入修改后的年龄：");
+            String newAge = sc.nextLine();
+            System.out.println("请输入修改后的居住的：");
+            String newAddress = sc.nextLine();
+
+            //创建学生对象
+            Student stu = array.get(index);
+            stu.setNumber(newNumber);
+            stu.setName(newName);
+            stu.setAge(newAge);
+            stu.setAddress(newAddress);
+
+            //修改成功的提示
+            System.out.println("修改成功！");
         }
     }
 }
